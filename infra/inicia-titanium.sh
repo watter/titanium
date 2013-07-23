6a966c3b69a6cd78fda0710624fda0e7570f82aa
Leslie Harlley Watter <watter@gmail.com>
2013-07-23 10:29:59 -0300

    inicialização

---
+++ infra/inicia-titanium.sh
@@ -0,0 +1,8 @@
+export http_proxy="http://proxyservico.eparana.parana:8181"
+export https_proxy="http://proxyservico.eparana.parana:8181"
+export NPM_CONFIG_PREFIX="/home/desenv/mobile/nodejs/"
+export PATH="/home/desenv/mobile/nodejs/bin:/home/desenv/mobile/nodejs/bin:/usr/lib/lightdm/lightdm:/usr/local/bin:/usr/bin:/bin:/usr/lib/jvm/jdk1.7.0_25/bin"
+export JAVA_HOME="/usr/lib/jvm/jdk1.7.0_25/"
+
+cd Titanium_Studio
+./TitaniumStudio
