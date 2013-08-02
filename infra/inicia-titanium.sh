7ced42a2af8f93b2b8e57099156510a7c9dc7876
Leslie Harlley Watter <watter@gmail.com>
2013-08-02 10:21:02 -0300

    script de inicialização do titanium com configuração de PATH para o meu usuário

---
+++ infra/inicia-titanium.sh
@@ -0,0 +1,8 @@
+export http_proxy="http://proxyservico.eparana.parana:8181"
+export https_proxy="http://proxyservico.eparana.parana:8181"
+export NPM_CONFIG_PREFIX="/home/desenv/mobile/nodejs/"
+export JAVA_HOME="/usr/lib/jvm/jdk1.7.0_25/"
+export PATH="/home/desenv/mobile/nodejs/bin:/home/desenv/mobile/mobile-env/android-sdks/platform-tools/:/home/desenv/mobile/mobile-env/android-sdks/tools/:/home/desenv/mobile/nodejs/bin:/usr/lib/lightdm/lightdm:/usr/local/bin:/usr/bin:/bin:/usr/lib/jvm/jdk1.7.0_25/bin"
+
+cd Titanium_Studio
+./TitaniumStudio
